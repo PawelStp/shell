0b0d83341365640e1af0480f0b895998d9c6598d
pawelstp <stypulkowski.pawel1@gmail.com>
2017-04-13 17:03:13 +0200

    Added history

--- main.c
+++ main.c
@@ -4,6 +4,30 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+volatile int flag_back=0;
+
+void handler(int signum)
+{
+
+}
+int cd(char **args)
+{
+    if(args[1]==NULL)
+    {
+        fprintf(stderr, "Nie podano folderu docelowego\n");
+    }
+    else
+    {
+        if(chdir(args[1])!=0)
+        {
+            perror("Brak podanego folderu");
+        }
+    }
+    return 1;
+}
+
+
 char* readArguments()
 {
 	char* line=NULL;
@@ -16,7 +40,7 @@ char** SepLine(char *line)
 	int bufforSize = 64, position = 0;
 	char** tokens = malloc(bufforSize * sizeof(char*));
 	char *token;
-	
+
 	token = strtok(line, " \n");
 	while(token!=NULL)
 	{
@@ -27,9 +51,15 @@ char** SepLine(char *line)
 			bufforSize += 64;
 			tokens = realloc(tokens, bufforSize * sizeof(char*));
 		}
-		token = strtok(NULL," \n");		
+		token = strtok(NULL," \n");
 
-	}	
+	}
+	token = tokens[position-1];
+	if(token[strlen(token)-1]=='&')
+	{
+        flag_back=1;
+        token[strlen(token)-1]=NULL;
+	}
 	tokens[position] = NULL;
 	return tokens;
 
@@ -48,12 +78,12 @@ int launch(char** args)
 	exit(EXIT_FAILURE);
 	}
 	else
-	{	
+	{
 		if(pid<0)
 		{
-			perror("Brak podanego programu!!!");			
+			perror("Brak podanego programu!!!");
 		}
-		else
+		else if(flag_back == 0)
 		{
 			do
 			{
@@ -61,18 +91,81 @@ int launch(char** args)
 			}while(!WIFEXITED(status) && !WIFSIGNALED(status));
 		}
 	}
-} 
+}
 
+void saveHistory(char *arg, char *cmd)
+{
+    int r=open(arg,O_RDONLY);
+    int s,i, max=0;
+    int j=0;
+    char c[1];
+    char history[20][100];
+    if(r != -1)
+    {
+        i = 0;
+        while(read(r, c, 1) && i < 19)
+        {
+            history[i][j]=c[0];
+            if(c[0] == '\n')
+            {
+                history[i][j]=NULL;
+                j=0;
+                i++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        max=i;
+        close(r);
+        s = open(arg, O_WRONLY | O_CREAT| O_TRUNC, 0666);
+        write(s, cmd, strlen(cmd));
+        write(s, "\n",1);
+        j=0; i=0;
+        while(1)
+        {
+
+            write(s, history[i], strlen(history[i]));
+            write(s, "\n",1);
+            i++;
+            if(i==max)break;
+        }
+        close(s);
+    }
+    else
+    {
+        s = open(arg, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+        write(s,cmd,strlen(cmd));
+        write(s, "\n",1);
+        close(s);
+    }
+
+}
 
 int main (int argc, char* argv[])
 {
-	//fprintf(stderr,"%s",argv[0]);
+    int a;
 	while(1)
 	{
 		char *line=readArguments();
 		char **args=SepLine(line);
-		int a = launch(args);
-	}
+
+		if(strcmp(args[0],"cd")==0)
+		{
+            saveHistory("h.txt", args[0]);
+            cd(args);
+		}
+		else
+		{
+		    saveHistory("h.txt", args[0]);
+            a = launch(args);
+		}
+        if(flag_back==1)
+		{
+            flag_back=0;
+		}
+    }
 
 
 return 0;
